ef1620c1e0632a464422d46592c74977b5fd1214
k3ll3x <k3ll3x@gmail.com>
2019-11-30 18:21:59 -0600

    working on chimeralib

--- chimera-phase5/chimeralib.cs
+++ chimera-phase5/chimeralib.cs
@@ -10,22 +10,26 @@ using System.Collections.Generic;
 
 namespace Chimera {
     public class Utils {
+        private static int currentId = 0;
+        private static Dictionary<int, List<int>> handles = new Dictionary<int, List<int>>();
+
         public static int WrInt(int n){
             Console.Write(i);
             return 0;
         }
 
-        public static int WrInt(string n){
-            Console.Write(i);
+        public static int WrStr(int s){
+            chHandle(s);
+            StringBuilder builder = new StringBuilder();
+            for(int i = 0, n = Size(s); i < n; i++){
+                builder.Append(char.ConvertFromUtf32(Get(s,i)));
+            }
+            Console.Write(builder.ToString());
             return 0;
         }
 
         public static int WrBool(int n){
-            if(n == 1){
-                Console.Write("true");
-            }else if(n == 0){
-                Console.Write("false")
-            }
+            Console.Write(n ? "true" : "false");
             return 0;
         }
 
@@ -45,6 +49,54 @@ namespace Chimera {
                 Add(n,i);
             }
             return n;
+        }
+
+        public static int LenStr(int h){
+            chHandle(h);
+            return handles[h].Count;
+        }
+
+        public static int New(int n){
+            if(n < 0){
+                throw new Exception("Error, negative size array!");
+            }
+            int h = currentId++;
+            handles.Add(h, new List<int>());
+            for(int i = 0; i < n; i++){
+                Add(h,0);
+            }
+            return h;
+        }
+
+        public static int Add(int h, int i){
+            chHandle(h);
+            handles[h].Add(i);
+            return 0;
+        }
+
+        private static void chHandle(int h){
+            if(!handles.ContainsKey(h)){
+                throw new Exception("Not valid array handle!");
+            }
+        }
+
+        public static int Size(int h){
+            chHandle(h);
+            return handles[h].Count;
+        }
+
+        public static int Get(int h, int i){
+            chHandle(h);
+            return handles[h][i];
+        }
+
+        private static IEnumerable<int> AsCodePoints(string str){
+            for(int i = 0; i < str.Length; i++){
+                yield return char.ConvertToUtf32(str, i);
+                if(char.IsHighSurrogate(str,i)){
+                    i++;
+                }
+            }
         }
      }
  }
